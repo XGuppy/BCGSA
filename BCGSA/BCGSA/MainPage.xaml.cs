a44e3c89aadf406514b0fd71d3bcf664b5c11f48
XGuppy <xguppy@yandex.ru>
2019-02-22 23:30:57 +0400

    GSensor Use

--- BCGSA/BCGSA/MainPage.xaml.cs
+++ BCGSA/BCGSA/MainPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using Xamarin.Essentials;
 namespace BCGSA
 {
     public partial class MainPage : ContentPage
@@ -12,6 +12,14 @@ namespace BCGSA
         public MainPage()
         {
             InitializeComponent();
+            Accelerometer.Start(SensorSpeed.Game);
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+        }
+
+        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+            lbl.Text = $"X:{data.Acceleration.X} Y:{data.Acceleration.Y} Z:{data.Acceleration.Z}";
         }
     }
 }
